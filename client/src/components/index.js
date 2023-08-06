f755b82390a3b52dcc0dd27b766a431f7beed647
Merwin Rebello <merwinrebello02@gmail.com>
2023-08-06 21:07:17 +0530

    created responsive nav bar for mobile devices

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Navbar } from "./Navbar";
 export { default as Sdiebar } from "./Sdiebar";
-export { default as Custombutton } from "./CustomButton";
+export { default as CustomButton } from "./CustomButton";
